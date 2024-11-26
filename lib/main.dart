2362b8c8fe0aafa7fe9c3cfae47725e0e7bb6f45
Deepak <trojanhorsedc@gmail.com>
2024-11-27 00:29:07 +0530

    Added home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_v2/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Bloc',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(body: Container(),),
+      home: const HomePage(),
     );
   }
 }
