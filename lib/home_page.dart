0a7bff46670a59171c9515db74954a4610b39b12
Deepak <trojanhorsedc@gmail.com>
2024-11-27 01:10:49 +0530

    BlocBuilder demo

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_v2/cubit/counter_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,12 +15,33 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(""),
+            // BlocBuilder: A widget that helps re-building the UI based on bloc state changes
+            BlocBuilder<CounterCubit, CounterState>(
+                // buildWhen: (prevState, currState) {
+                //   // Return true/false to determine whether or not to rebuild the widget with state
+                //   return true;
+                // },
+                builder: (context, state) {
+              // return widget here based on Bloc State
+              // the builder function can be called MULTIPLE TIMES (bcoz of flutter engine)
+              return Text(
+                "${state.counterValue}",
+                style: const TextStyle(fontSize: 30),
+              );
+            }),
+            const SizedBox(
+              height: 20,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    // // Without extensions
+                    // BlocProvider.of<CounterCubit>(context).decrement();
+                    // // with extensions
+                    context.read<CounterCubit>().decrement();
+                  },
                   tooltip: 'Decrement',
                   child: const Icon(Icons.remove),
                 ),
@@ -26,7 +49,12 @@ class HomePage extends StatelessWidget {
                   width: 24,
                 ),
                 FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    // // Without extensions
+                    // BlocProvider.of<CounterCubit>(context).increment();
+                    // // with extensions
+                    context.read<CounterCubit>().increment();
+                  },
                   tooltip: 'Increment',
                   child: const Icon(Icons.add),
                 )
