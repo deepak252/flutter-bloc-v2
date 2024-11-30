5ea6e019e8c30e96176c5322ef8e30cadf2d022c
Deepak <trojanhorsedc@gmail.com>
2024-11-30 14:28:31 +0530

    Global Access Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_v2/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_v2/presentation/router/app_router.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // final _counterCubit = CounterCubit();
   final _appRouter = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Bloc',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider<CounterCubit>(
+      create: (_) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Bloc',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      onGenerateRoute: _appRouter.onGenerateRoute,
-      // routes: {
-      //   '/': (context) => BlocProvider.value(
-      //         value: _counterCubit,
-      //         child: const HomeScreen(),
-      //       ),
-      //   '/second': (context) => BlocProvider.value(
-      //         value: _counterCubit,
-      //         child: const SecondScreen(),
-      //       )
-      // },
-      // home: BlocProvider<CounterCubit>(
-      //     create: (_) => CounterCubit(), child: const HomeScreen()),
     );
-    // return BlocProvider<CounterCubit>(
-    //   create: (_) => CounterCubit(),
-    //   child: MaterialApp(
-    //     title: 'Flutter Bloc',
-    //     debugShowCheckedModeBanner: false,
-    //     theme: ThemeData(
-    //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-    //       useMaterial3: true,
-    //     ),
-    //     home: const HomeScreen(),
-    //   ),
-    // );
-  }
-
-  @override
-  void dispose() {
-    // _counterCubit.close();
-    _appRouter.dispose();
-    super.dispose();
   }
 }
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,32 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc_v2/logic/cubit/counter_cubit.dart';
 import 'package:flutter_bloc_v2/presentation/screens/home_screen.dart';
 import 'package:flutter_bloc_v2/presentation/screens/second_screen.dart';
 
 class AppRouter {
-  final _counterCubit = CounterCubit();
   Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
-        return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: const HomeScreen(),
-                ));
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       case '/second':
-        return MaterialPageRoute(
-            builder: (_) => BlocProvider.value(
-                  value: _counterCubit,
-                  child: const SecondScreen(),
-                ));
+        return MaterialPageRoute(builder: (_) => const SecondScreen());
 
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
